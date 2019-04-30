8031d0c5a68b2a7cd01f2e4784e9b0bdc54cae9f
Vaerilius <tjohannest@gmail.com>
2019-04-30 13:18:45 +0300

    Recipe class + creating objects from Recipe class

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +9,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is Test',
+    'https://www.tasteofhome.com/wp-content/uploads/2017/10/Chicken-Pizza_exps30800_FM143298B03_11_8bC_RMS-2-696x696.jpg'),
+    new Recipe('A Test Recipe', 'This is Test',
+    'https://www.tasteofhome.com/wp-content/uploads/2017/10/Chicken-Pizza_exps30800_FM143298B03_11_8bC_RMS-2-696x696.jpg'),
+    new Recipe('A Test Recipe', 'This is Test',
+    'https://www.tasteofhome.com/wp-content/uploads/2017/10/Chicken-Pizza_exps30800_FM143298B03_11_8bC_RMS-2-696x696.jpg')
+ 
+ 
+  ];
 
   constructor() { }
 
