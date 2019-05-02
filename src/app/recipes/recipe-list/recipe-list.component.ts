06cdba5d0443a030d31e40c8167906318647aecf
Vaerilius <tjohannest@gmail.com>
2019-05-02 19:51:24 +0300

    1.0 ready

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
-
-
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
@@ -16,8 +14,6 @@ export class RecipeListComponent implements OnInit {
     'https://www.tasteofhome.com/wp-content/uploads/2017/10/Chicken-Pizza_exps30800_FM143298B03_11_8bC_RMS-2-696x696.jpg'),
     new Recipe('A Test Recipe', 'This is Test',
     'https://www.tasteofhome.com/wp-content/uploads/2017/10/Chicken-Pizza_exps30800_FM143298B03_11_8bC_RMS-2-696x696.jpg')
- 
- 
   ];
 
   constructor() { }
